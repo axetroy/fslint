7b00d56a301d5820868843d519cf1fa88989f4fe
axetroy <axetroy.dev@gmail.com>
2021-03-05 09:42:38 +0800

    perf: improve cache binary for speed up install

--- npm/scripts/postinstall.js
+++ npm/scripts/postinstall.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const fs = require("fs");
 const os = require("os");
+const crypto = require("crypto");
 const download = require("download");
 const ProgressBar = require("progress");
 
@@ -43,31 +44,50 @@ function getDownloadURL(version) {
 
 async function install(version) {
   const url = getDownloadURL(version);
-
-  console.log(`Downloading '${url}'`);
-
-  const bar = new ProgressBar("[:bar] :percent :etas", {
-    complete: "=",
-    incomplete: " ",
-    width: 20,
-    total: 0,
-  });
-
   const binDir = path.join(__dirname, "..", "download");
-
-  await download(url, binDir, {
-    extract: true,
-  }).on("response", (res) => {
-    bar.total = res.headers["content-length"];
-    res.on("data", (data) => bar.tick(data.length));
-    res.on("error", (err) => {
-      console.error("error");
-    });
-  });
-
-  // make it become a executable file
   const suffix = os.platform() === "win32" ? ".exe" : "";
   const binFile = path.join(binDir, "fslint" + suffix);
+
+  const cacheHex = crypto.createHash("md5").update(url).digest("hex");
+  const cachePath = path.join(os.tmpdir(), cacheHex);
+
+  try {
+    fs.unlinkSync(binFile);
+  } catch {
+    // ignore error
+  }
+
+  if (fs.existsSync(cachePath)) {
+    console.log(`Found cache in '${cachePath}'`);
+    fs.copyFileSync(cachePath, binFile);
+  } else {
+    console.log(`Downloading '${url}'`);
+    const bar = new ProgressBar("[:bar] :percent :etas", {
+      complete: "=",
+      incomplete: " ",
+      width: 20,
+      total: 0,
+    });
+
+    await download(url, binDir, {
+      extract: true,
+    })
+      .on("response", (res) => {
+        bar.total = res.headers["content-length"];
+        res.on("data", (data) => bar.tick(data.length));
+        res.on("error", (err) => {
+          console.error(err);
+        });
+      })
+      .on("error", (err) => {
+        console.error(err);
+      });
+
+    console.log(`Cache binary file to '${cachePath}'`);
+
+    fs.copyFileSync(binFile, cachePath);
+  }
+
   fs.chmod(binFile, 0x755, (err) => {
     // ignore error
     if (err) {
